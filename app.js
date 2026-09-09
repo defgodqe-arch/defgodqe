fa739883bd09712b3ef0b2b6cc541c9e88e2a601
defgodqe-arch <defgodqe@gmail.com>
2026-09-09 16:29:23 -0500

    Restore defgodqe branding logo

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-/* defgodqe — restored working core version */
+/* defgodqe — restored working core + branding */
 import 'https://cdn.jsdelivr.net/gh/defgodqe-arch/defgodqe@013d26575d03aebd4c6dbc6bb62427f5650d2a93/app.js';
+import './branding.js';
